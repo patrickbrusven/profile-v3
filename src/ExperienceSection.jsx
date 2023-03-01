dd8c7230c692673a782b76f29c2cec8360748fb0
patrickbrusven <pjbrusven@gmail.com>
2023-02-28 16:08:49 -0800

    experience section

---
+++ src/ExperienceSection.jsx
@@ -0,0 +1,24 @@
+function ExperienceSection() {
+  return (
+    <div className="experience-section">
+      <div>
+        <h5 className="subheading-text">Experience</h5>
+      </div>
+      <ul>
+        <li>Open Source</li>
+        <li>Sparc Agency</li>
+        <li>Freelance</li>
+      </ul>
+      <div className="experience-section__card">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+          aperiam deserunt repudiandae unde minima. Perspiciatis laudantium
+          atque fugit non quod ipsam distinctio nobis, voluptates ab vero earum,
+          dolore reiciendis dolorem.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ExperienceSection;
